844ff133ecc11bcac723bc4adc0129315d0251b4
John Joyce <jjoyce6@nd.edu>
2016-12-04 16:14:33 -0500

    a lot of stuff

--- kdtree.cpp
+++ kdtree.cpp
@@ -5,58 +5,152 @@
 // #include <cassert>
 using namespace std;
 
-// template<Typename T>
+template<typename T>
 class kd_node {
 	public:
-	vector<int> values;
-	kd_node* left;
-	kd_node* right;
+	vector<T> values;
+	kd_node<T> *left;
+	kd_node<T> *right;
+
 	void print() {
-		for (vector<int>::iterator it = values.begin(); it < values.end(); it++) cout << *it << " ";
+		for (typename vector<T>::iterator it = values.begin(); it < values.end(); it++) cout << *it << " ";
 		cout << endl;
 	}
+
 	kd_node() { left = nullptr; right = nullptr; }
 	~kd_node() { delete left; delete right; }
 };
 
-// template<typename T>
+template<typename T>
+class kd_range {
+	friend ostream& operator<<(ostream &os, kd_range &r) {
+		os << "(";
+		for (int i = 0; i < r.dimensions; i++) {
+			os << r.lower[i] << "..." << r.upper[i];
+		}
+		os << ")";
+		return os;
+	}
+
+	public:
+	vector<T> lower;
+	vector<T> upper;
+
+	kd_range() : dimensions(0) {}
+	kd_range(vector<T> &lower, vector<T> &upper) {
+		if (lower.size() < upper.size()) dimensions = lower.size();
+		else dimensions = upper.size();
+
+		lower = lower;
+		upper = upper;
+
+	}
+
+	bool contains(kd_node<T> *&n) {
+		return contains(n->values);
+	}
+
+	bool contains(vector<T> &n) {
+		for (int i = 0; i < dimensions; i++) {
+			if (n[i] < lower[i] && n[i] > upper[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	~kd_range() {};
+	int dimension() const { return dimensions; }
+
+	private:
+	int dimensions;
+
+};
+
+template <typename T>
+class range_node {
+	public:
+	kd_node<T> node;
+	int distance;
+};
+
+template <typename T>
+struct range_node_compare {
+	bool operator()(const range_node<T> &r1, const range_node<T> &r2) {
+		return r1.distance < r2.distance;
+	}
+};
+
+template<typename T>
 class kd_tree {
 public:
 	kd_tree() : dimensions(2), SIZE(0) { root = nullptr; }
 	kd_tree(int d) :  SIZE(0) {
-		if (d < 1) dimensions = 2;
+		if (d <= 1) dimensions = 2;
 		else dimensions = d;
 		root = nullptr;
 	}
+
 	~kd_tree() { delete root; }
-	void insert(vector<int> &new_values) {
+
+	//insert into KD tree
+	void insert(vector<T> &new_values) {
 		if (new_values.size() != dimensions) return;
 		if (root == nullptr) {
-			kd_node* new_node = new kd_node;
+			kd_node<T> *new_node = new kd_node<T>();
 			new_node->values = new_values;
 			root = new_node;
 			SIZE++;
 		} else insert_r(root, new_values, 0);
 	}
 
-	vector<int> nearest_neighbor(vector<int> &n) {
-		int orientation = 0;
+	//search KD tree based on coordinate values
+	kd_node<T>* search(vector<T> &search_vals) {
+		if (search_vals.size() != dimensions) return nullptr;
+		if (root == nullptr) return nullptr;
+
+		return search_r(root, search_vals, 0);
+
+	}
+
+	//find nearest neighbor to KD tree by vector
+	kd_node<T>* nearest_neighbor(vector<T> &n) {
 		int max = INT_MAX;
-		kd_node* nearest = nullptr;
-		nearest_neighbor_r(root, n, orientation, max, nearest);
-		if (nearest == nullptr) return vector<int>();
-		return nearest->values;
+		kd_node<T>* nearest = nullptr;
+		nearest_neighbor_r(root, n, 0, max, nearest);
+		return nearest;
+	}
+
+	//find nearest neighbor to KD by node
+	kd_node<T>* nearest_neighbor(kd_node<T>* &n) {
+		kd_node<T>* nearest = nullptr;
+		int max = INT_MAX;
+		nearest_neighbor_r(root, n->values, 0, max, nearest);
+		return nearest;
+	}
+
+	//get all nodes falling with in a range specified by a kd_range object
+	vector<kd_node<T>*> range_search(kd_range<T> &r) {
+		vector<kd_node<T>* > results;
+
+		return results;
+
+	}
+
+	vector<kd_node<T>*> range_search(vector<T> &l, vector<T> &u) {
+		kd_range<T> r(l, u);
+		return range_search(r);
 	}
 
 	void balance() {
-		/* 
+		/*
 		get all kd_nodes in list; O(n)
 		delete root;
 		2 choices:
-			1. pick random kd_nodes and insert them (probably will be balanced, but fast) O(n)
+			1. pick random kd_nodes and insert them (probably will not be balanced, but fast) O(n)
 			2. find median and do inserts with median (balanced, but slower) O(nlog(n))
 		we could create both and bench mark them too...
-		*/ 
+		*/
 	}
 
 	void print() { print_r(root); }
@@ -64,16 +158,18 @@ public:
 	int dimension() const { return dimensions; }
 
 private:
-	kd_node* root;
+	kd_node<T>* root;
 	int dimensions;
 	size_t SIZE;
-	void insert_r(kd_node* root, vector<int> &new_values, int orientation) {
+
+	//recursively insert into KD tree
+	void insert_r(kd_node<T>* root, vector<T> &new_values, int orientation) {
 		if (root == nullptr) return;
 		orientation %= dimensions;
 		// normal bst insertion except we look at a specific element of the values vector
 		if (new_values[orientation] < root->values[orientation]) {
 			if (root->left == nullptr) {
-				kd_node* new_node = new kd_node;
+				kd_node<T> *new_node = new kd_node<T>();
 				root->left = new_node;
 				new_node->values = new_values;
 				SIZE++;
@@ -84,7 +180,7 @@ private:
 		}
 		else if (new_values[orientation] >= root->values[orientation]) {
 			if (root->right == nullptr) {
-				kd_node* new_node = new kd_node;
+				kd_node<T> *new_node = new kd_node<T>();
 				root->right = new_node;
 				new_node->values = new_values;
 				SIZE++;
@@ -92,11 +188,29 @@ private:
 			} else  {
 				insert_r(root->right, new_values, orientation + 1);
 			}
-		} 
+		}
 		return;
 	}
 
-	void print_r(kd_node* root) {
+	//recursively search KD tree
+	kd_node<T>* search_r(kd_node<T> *root, vector<T> &search_vals, int orientation) {
+		if (root == nullptr) return nullptr;
+
+		orientation %= dimensions;
+
+		//normal bst search. -- all elements must match
+		if (search_vals[orientation] < root->values[orientation])
+			return search_r(root->left, search_vals, orientation+1);
+		else if (search_vals[orientation] == root->values[orientation])
+			if (compare_values(search_vals, root->values)) return root;
+			else return search_r(root->right, search_vals, orientation+1);
+		else
+			return search_r(root->right, search_vals, orientation+1);
+
+	}
+
+	//print in order
+	void print_r(kd_node<T>* root) {
 		// print value in tree using in_order traversal
 		if (root == nullptr) return;
 		print_r(root->left);
@@ -108,14 +222,15 @@ private:
 		print_r(root->right);
 	}
 
-	void nearest_neighbor_r(kd_node* &root, vector<int> &n, int orientation, int &min_dist, kd_node* &near) {
+	//recursively find nearest neighbor
+	void nearest_neighbor_r(kd_node<T>* &root, vector<T> &n, int orientation, int &min_dist, kd_node<T>* &near) {
 		if (root == nullptr) return;
 		// root->print();
 		if (root->left == nullptr && root->right == nullptr) {
 			int d = dist(root->values, n);
 			// only set near = root if its distance to target is less than min_dist
-			// this will automatically happen for first leaf we hit 
-			if (d < min_dist) {
+			// this will automatically happen for first leaf we hit -- right
+			if (d < min_dist && d != 0) {
 				min_dist = d;
 				near = root;
 			}
@@ -133,7 +248,7 @@ private:
 		}
 
 		int d = dist(root->values, n);
-		if (d < min_dist) {
+		if (d < min_dist && d != 0) {
 			min_dist = d;
 			near = root;
 		}
@@ -153,8 +268,7 @@ private:
 		}
 	}
 
-	int dist(vector<int> &a, vector<int> &b) {
-
+	int dist(vector<T> &a, vector<T> &b) {
 		//validate sizes
 		int sizeA = a.size();
 		int sizeB = b.size();
@@ -166,4 +280,16 @@ private:
 		// squared sum prevents us from computing sqrt alot
 		return sum;
 	}
+
+	bool compare_values(vector<T> &a, vector<T> &b) {
+		if (a.size() != b.size()) return false;
+		if (a.size() != dimensions) return false;
+
+		for (int i = 0; i < dimensions; i++) {
+			if (a[i] != b[i]) return false;
+		}
+
+		return true;
+	}
+
 };
--- test.t.cpp
+++ test.t.cpp
@@ -4,18 +4,18 @@
 #include "kdtree.cpp"
 
 TEST_CASE("Constructor Tests", "[kdtree]") {
-	kd_tree k(2);
+	kd_tree <int> k(2);
 	REQUIRE(k.dimension() == 2);
 
-	kd_tree j(-1);
+	kd_tree <int> j(-1);
 	REQUIRE(k.dimension() == 2);
 
-	kd_tree l(0);
-	REQUIRE(k.dimension() == 2);	
+	kd_tree <int> l(0);
+	REQUIRE(k.dimension() == 2);
 }
 
 TEST_CASE( "Insert Tests", "[kdtree::insert]" ){
-    kd_tree k(2);
+    kd_tree <int> k(2);
     vector<int> v;
     REQUIRE(v.size() == 0);
     v.push_back(4);
@@ -50,29 +50,89 @@ TEST_CASE( "Insert Tests", "[kdtree::insert]" ){
 
 }
 
-TEST_CASE("NN Algorithm Tests", "[kdtree::nearest_neighbor]") {
-	kd_tree k;
+TEST_CASE("Search Tests", "[kdtree::search]") {
+	kd_tree <int> k(2);
+	vector<int> s;
+	s.push_back(5);
+	s.push_back(4);
+	vector<int> s2;
+	s2.push_back(2);
+	s2.push_back(2);
+
+	SECTION("Empty Tree") {
+		kd_node<int> *result = k.search(s);
+		REQUIRE(result == 0);
+	}
+
 	vector<int> v;
-	v.push_back(1); 
+	v.push_back(2);
+	v.push_back(4);
+	vector<int> v1;
+	v1.push_back(5);
+	v1.push_back(4);
+
+	k.insert(v);
+	k.insert(v1);
+
+	SECTION("No match - same first term") {
+		kd_node<int> *result = k.search(s2);
+
+		REQUIRE(result == 0);
+	}
+
+	SECTION("No match - no matching terms") {
+		vector<int> s3;
+		s3.push_back(17);
+		s3.push_back(18);
+		kd_node<int> *result = k.search(s3);
+
+		REQUIRE(result == 0);
+	}
+
+	SECTION("Match - leaf node") {
+		kd_node<int> *result = k.search(s);
+
+		REQUIRE(result->values[0] == 5);
+		REQUIRE(result->values[1] == 4);
+	}
+
+	SECTION("Match - root node") {
+		kd_node<int> *result = k.search(v);
+
+		REQUIRE(result->values[0] == 2);
+		REQUIRE(result->values[1] == 4);
+	}
+}
+
+TEST_CASE("NN Algorithm Tests", "[kdtree::nearest_neighbor]") {
+	kd_tree<int> k;
+	vector<int> v;
+	v.push_back(1);
 	v.push_back(2);
 
 	SECTION("Empty tree") {
-		vector<int> c = k.nearest_neighbor(v);
-		REQUIRE(c.size() == 0);
+		kd_node<int> *c = k.nearest_neighbor(v);
+		REQUIRE(c == 0);
 	}
 
-	SECTION("NN search w/ kd_tree.size() == 1") {
+
+	SECTION("NN vector search w/ kd_tree.size() == 1") {
 		k.insert(v);
 		vector<int> c;
 		c.push_back(2); c.push_back(3);
-		vector<int> d = k.nearest_neighbor(v);
-		REQUIRE(d[0] == v[0]);
-		REQUIRE(d[1] == v[1]);
+		kd_node<int> *d = k.nearest_neighbor(c);
+		REQUIRE(d->values[0] == v[0]);
+		REQUIRE(d->values[1] == v[1]);
 	}
 
-	SECTION("NN search where leaf is NN") {
+	SECTION("NN vector search where NN node itself") {
+		kd_node<int> *d = k.nearest_neighbor(v);
+		REQUIRE(d == 0);
+	}
+
+	SECTION("NN vector search where leaf is NN") {
 		vector<int> c;
-		c.push_back(4); c.push_back(5); 
+		c.push_back(4); c.push_back(5);
 		k.insert(c); // root
 
 		c[0] = 2; c[1] = 6; // left
@@ -88,13 +148,14 @@ TEST_CASE("NN Algorithm Tests", "[kdtree::nearest_neighbor]") {
 		k.insert(c);
 
 		c[0] = 3; c[1] = 1; // should be closest to 3,3 ?
-		vector<int> d = k.nearest_neighbor(c);
+		kd_node<int> *d = k.nearest_neighbor(c);
 
-		REQUIRE(d[0] == 3);
-		REQUIRE(d[1] == 3);
+		REQUIRE(d->values[0] == 3);
+		REQUIRE(d->values[1] == 3);
+
 	}
 
-	SECTION("NN search where root is NN") {
+	SECTION("NN vector search where root is NN") {
 		vector<int> c;
 		c.push_back(1); c.push_back(2);
 		k.insert(c);
@@ -115,13 +176,13 @@ TEST_CASE("NN Algorithm Tests", "[kdtree::nearest_neighbor]") {
 		k.insert(c);
 
 		c[0] = 1; c[1] = 1;
-		vector<int> d = k.nearest_neighbor(c); // should be 1,2
+		kd_node<int> *d = k.nearest_neighbor(c); // should be 1,2
 
-		REQUIRE(d[0] == 1);
-		REQUIRE(d[1] == 2);
+		REQUIRE(d->values[0] == 1);
+		REQUIRE(d->values[1] == 2);
 	}
 
-	SECTION("NN search where NN is non leaf or root") {
+	SECTION("NN vector search where NN is non leaf or root") {
 		vector<int> c;
 		c.push_back(1); c.push_back(2);
 		k.insert(c);
@@ -142,15 +203,15 @@ TEST_CASE("NN Algorithm Tests", "[kdtree::nearest_neighbor]") {
 		k.insert(c);
 
 		c[0] = 7; c[1] = 5;
-		vector<int> d = k.nearest_neighbor(c); // should be 8,5
+		kd_node<int> *d = k.nearest_neighbor(c); // should be 8,5
 
-		REQUIRE(d[0] == 8);
-		REQUIRE(d[1] == 5);
+		REQUIRE(d->values[0] == 8);
+		REQUIRE(d->values[1] == 5);
 	}
 
-	SECTION("NN search switching subtrees from left to right") {
+	SECTION("NN vector search switching subtrees from left to right") {
 		vector<int> c;
-		c.push_back(4); c.push_back(5); 
+		c.push_back(4); c.push_back(5);
 		k.insert(c); // root
 
 		c[0] = 2; c[1] = 6; // left
@@ -161,7 +222,7 @@ TEST_CASE("NN Algorithm Tests", "[kdtree::nearest_neighbor]") {
 
 		c[0] = 4; c[1] = 1; // right, left
 		k.insert(c);
- 
+
 		c[0] = 3; c[1] = 3; // left, left
 		k.insert(c);
 
@@ -169,11 +230,37 @@ TEST_CASE("NN Algorithm Tests", "[kdtree::nearest_neighbor]") {
 		k.insert(c);
 
 		c[0] = 3; c[1] = 1; // should be closest to 4,1
-		vector<int> d = k.nearest_neighbor(c);
+		kd_node<int> *d = k.nearest_neighbor(c);
 
-		REQUIRE(d[0] == 4);
-		REQUIRE(d[1] == 1);
+		REQUIRE(d->values[0] == 4);
+		REQUIRE(d->values[1] == 1);
+	}
+
+	SECTION("NN node search switching subtrees from left to right") {
+		vector<int> c;
+		c.push_back(4); c.push_back(5);
+		k.insert(c); // root
+
+		c[0] = 2; c[1] = 6; // left
+		k.insert(c);
+
+		c[0] = 5; c[1] = 10; // right
+		k.insert(c);
+
+		c[0] = 4; c[1] = 1; // right, left
+		k.insert(c);
+
+		c[0] = 3; c[1] = 3; // left, left
+		k.insert(c);
+
+		c[0] = 2; c[1] = 8; // left, right
+		k.insert(c);
+		kd_node<int> *res = k.search(c);
+
+		kd_node<int> *d = k.nearest_neighbor(res);
+
+		REQUIRE(d->values[0] == 2);
+		REQUIRE(d->values[1] == 6);
 	}
 
 }
-
